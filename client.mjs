a23d9ddd659262a60bd291836c0d30082a6bd4d8
Colin McNeil <colinmcneil@gmail.com>
2023-02-24 13:04:35 -0500

    Implement command handling

--- client.mjs
+++ client.mjs
@@ -1,28 +1,29 @@
 import WebSocket from "ws";
 
-// ws.on("error", console.error);
-
-// ws.on("message", function message(data) {
-//   console.log("received: %s", data);
-// });
-
 class Client {
   ws = null;
   constructor(symbols = ["BTC-USD", "ETH-USD", "XRP-USD", "LTC-USD"]) {
     this.ws = new WebSocket("wss://ws-feed.exchange.coinbase.com ");
+    this.ws.on("error", console.error);
     this.ws.on("open", () => {
-      ws.send({
-        type: "subscribe",
-        product_ids: symbols,
-        channels: [
-          "level2",
-          "heartbeat",
-          {
-            name: "ticker",
-            product_ids: symbols,
-          },
-        ],
-      });
+      this.ws.send(
+        JSON.stringify({
+          type: "subscribe",
+          product_ids: symbols,
+          channels: [
+            "level2",
+            "heartbeat",
+            {
+              name: "ticker",
+              product_ids: symbols,
+            },
+          ],
+        })
+      );
+    });
+    this.ws.on("message", function message(data) {
+      const parsedResponse = JSON.parse(data) || {};
+      // console.log("received from coinbase: %s", parsedResponse);
     });
   }
 }
--- server.mjs
+++ server.mjs
@@ -4,20 +4,43 @@ import Client from "./client.mjs";
 const wss = new WebSocketServer({ port: 3000 });
 
 const commands = {
-  quit() {},
-  m() {},
-  u() {},
-  symbol() {},
+  quit(client, ws) {
+    ws.send("Command - quit");
+  },
+  symbol: {
+    price(client, ws, symbol) {
+      ws.send(`Command - symbol price for ${symbol}`);
+    },
+    m(client, ws, symbol) {
+      ws.send(`Command - symbol matches for ${symbol}`);
+    }, // Matches view
+    u(client, ws, symbol) {
+      ws.send(`Command - symbol unsub to ${symbol}`);
+    }, // Unsubscribe
+  },
+  system(client, ws, interval = null) {
+    ws.send(`Command - system (interval: ${interval})`);
+  },
 };
 
-const client = new Client();
+const CoinBaseClient = new Client();
 
 wss.on("connection", function connection(ws) {
   ws.on("error", console.error);
 
   ws.on("message", function message(data) {
-    console.log("received: %s", data);
+    data = data.toString();
+    const command = data.split(" ")[0];
+    if (Object.keys(commands).includes(command)) {
+      commands[command](CoinBaseClient, ws, data.split(" ")[1]);
+    } else if (command.match(/[A-Z]{3}-[A-Z]{3}/)) {
+      console.log("Found symbol");
+      const symbolCommand = data.split(" ")[1] || "price";
+      commands.symbol[symbolCommand](CoinBaseClient, ws, command);
+    } else {
+      ws.send("Invalid command or invalid symbol (case sensitive)");
+    }
   });
 
-  ws.send("something");
+  ws.send("Connected to coinbase socket middleware.");
 });
